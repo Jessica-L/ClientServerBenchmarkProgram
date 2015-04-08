f931bf9c1105792476393ee9f4012bfe70b143c8
Jessica Lynch <jessica.lynch@colorado.edu>
2015-04-07 20:43:00 -0600

    changed folder name for shared memory C code

--- SharedMemoryJNI.java
+++ SharedMemoryJNI.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Project: Client Server Performance Measurement Program
+ * Authors: Jessica Lynch and Andrew Arnopoulos
+ * Date:    27-Apr-2015
  */
 package clientserver;
 
